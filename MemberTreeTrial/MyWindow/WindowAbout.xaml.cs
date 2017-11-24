bc102732447fdfac497cd9a8b30277de9acd9dd1
TomChen <cjtlp2006@163.com>
2017-11-24 11:49:07 +0800

    trial version 10 complete

--- MemberTreeTrial/MyWindow/WindowAbout.xaml.cs
+++ MemberTreeTrial/MyWindow/WindowAbout.xaml.cs
@@ -11,6 +11,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 
 namespace MemberTree
 {
@@ -24,34 +28,22 @@ namespace MemberTree
 			InitializeComponent();
 		}
 		
-		void BtnLocalNet_Click(object sender, RoutedEventArgs e)
+		void BtnOpenImg_Click(object sender, RoutedEventArgs e)
 		{
-
+			Button btn = sender as Button;
+			string imgUri = "/MemberTreeTrial;component/Image/" + btn.Name + ".png";
+			imgCover.Source = new BitmapImage(new Uri(imgUri, UriKind.Relative));
+			imgCover.OpacityMask = this.Resources["OpenBrush"] as LinearGradientBrush;
+		    Storyboard std = this.Resources["OpenStoryboard"] as Storyboard;
+		    std.Begin();
 		}
 		
-		void BtnAuthorize_Click(object sender, RoutedEventArgs e)
+		void ImgCover_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
 		{
-
-		}
-		
-		void BtnExport_Click(object sender, RoutedEventArgs e)
-		{
-
-		}
-		
-		void BtnImport_Click(object sender, RoutedEventArgs e)
-		{
-
-		}
-		
-		void BtnDynamicCol_Click(object sender, RoutedEventArgs e)
-		{
-
-		}
-		
-		void BtnQuantity_Click(object sender, RoutedEventArgs e)
-		{
-
+			imgCover.OpacityMask = this.Resources["CloseBrush"] as LinearGradientBrush;
+		    Storyboard std = this.Resources["CloseStoryboard"] as Storyboard;
+		    std.Completed += delegate { imgCover.Source = null; };
+		    std.Begin();
 		}
 	}
 }
